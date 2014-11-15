e3149aa002e681908d51d36b92ad7101a5a5f445
Madis <madispiigli@gmail.com>
2014-11-15 14:24:37 +0200

    TEST

--- SQLite/SQL 1.py
+++ SQLite/SQL 1.py
@@ -10,20 +10,28 @@ def kasutaja_kuupaev():
     kasutaja_sisestab = input("Sisesta kuupäev formaadis pp.kk.aaaa: ")
     kuupaeva_format = datetime.strptime(kasutaja_sisestab, "%d.%m.%Y")
     kuuapaev_timestamp = str((time.mktime(kuupaeva_format.timetuple())))
+
     for i in kuuapaev_timestamp:
         timestamp_listi.append(i)
+
     timestamp_algus = timestamp_listi[5]
     kuupaev = str("".join(timestamp_algus))
     return kuupaev
+
+def siseta_kasutaja():
+    kasutaja_sisestab_kasutaja = input("Sisesta autor: ")
+    return kasutaja_sisestab_kasutaja
 
 conn = sqlite3.connect('main.db')
 c= conn.cursor()
 
 def loen_baasist_ridu():
-    sql = ("SELECT x.body_xml FROM messages x where x.timestamp LIKE '%s'" % (kasutaja_kuupaev() + '%'))
+    sql = ("SELECT x.body_xml FROM messages x where x.timestamp LIKE '%s' and x.from_dispname LIKE '%s'" % (kasutaja_kuupaev() + '%', siseta_kasutaja()))
     for row in c.execute(sql):
         print(row)
     c.close()
 
+#print (c.fetchone())
 if __name__ == "__main__":
     print(loen_baasist_ridu())
+
